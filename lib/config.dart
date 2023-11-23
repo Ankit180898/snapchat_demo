e9a92c7a89b337bcb8f1cc292761184b8a6324ce
Ankit180898 <karan180898@gmail.com>
2023-11-23 11:56:44 +0530

    Initial commit

---
+++ lib/config.dart
@@ -0,0 +1,9 @@
+import 'package:camera_deep_ar/camera_deep_ar.dart';
+
+class config{
+  static final newConfig=DeepArConfig(
+  androidKey:
+  '14f8ab275d5a821ec0660dc31ada12f265a11835dbb2728dba241b9e25047441ab70fdbafcdd41c2',
+  ioskey:
+  'e5dad1e08b04f2350eff138f3a01530fc65b0e602a90084a6c7a6bb5eb9d6a0d8babbc475f275797');
+}
